8ca853719f35fb3f3652886e0720f51b7f33c741
shobhit <singh@gmail.com>
2022-06-08 16:08:41 +0530

    updated

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,9 +1,31 @@
 <template>
 <div>
+    <TheHeader @sidenavToggle="displaySidenav = !displaySidenav"/>
+    <TheSidenav
+    :show="dislaySidenav"
+    @close="displaySidenav = false"/>
     <nuxt/>
 </div>
 </template>
+<script>
+import TheHeader from '@/components/Navigation/TheHeader'
+import TheSidenav from '@/components/Navigation/TheSidenav'
 
+export default{
+    components: {
+        TheHeader,
+        TheSidenav
+},
+data() {
+    return {
+        displaySidenav: false
+    }
+
+}
+
+}
+
+</script>
 <style>
 html {
     font-family: sans-serif;
